42bb9dc19f920c0989f1a67d8aa915d352514bf0
drownedout <danielcdonnelly@gmail.com>
2018-08-29 14:18:08 -0700

    Scaffolded component files, wired up ConnectedRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
 import { store, history } from './redux/store';
 
 ReactDOM.render((
     <Provider store={store}>
-        <App />
+    	<ConnectedRouter history={history}>
+    		<Switch>
+    			<Route path="/" component={App} />
+    		</Switch>
+    	</ConnectedRouter>
     </Provider>
 ), document.getElementById('root'));
 
