3d15fd0cbf1339d4b6109e65e8401d50b73baade
drownedout <danielcdonnelly@gmail.com>
2018-08-29 15:03:03 -0700

    Added check for user auth in index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
-import App from './App.js';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { store, history } from './redux/store';
+import { getUser } from './redux/actions/actions';
+
+if(localStorage.Auth){
+	// Update Local Storage
+	store.dispatch({ type: 'SET_USER', user: JSON.parse(localStorage.Auth) });
+
+	let _id = JSON.parse(localStorage.Auth)._id;
+
+	getUser(_id)
+	.then((res) => {
+		store.dispatch({ type: 'SET_USER', user: res })
+	})
+}
 
 ReactDOM.render((
     <Provider store={store}>
