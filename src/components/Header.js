be60aeaa8091e85e1420c00176fd638234d327fb
drownedout <danielcdonnelly@gmail.com>
2018-08-29 16:22:55 -0700

    Added base header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,31 @@ import { connect } from 'react-redux';
 class Header extends Component {
 	render(){
 		return (
-			<div>
-			</div>
+			<header className="header">
+				<div className="nav-wrapper">
+					<div className="nav-right">
+						<div className="logo">
+							<h1>Logo</h1>
+						</div>
+					</div>
+					<div className="nav-left">
+						<nav className="navbar">
+							<ul className="navbar-nav">
+								<a href="#" className="btn secondary">Sign In / Sign Up</a>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
 		)
 	}
 }
 
-export default Header;
+const mapStateToProps = state => {
+    return {
+        user: state.authUser.user,
+        isAuth: state.authUser.isAuth
+    }    
+}
+
+export default connect(mapStateToProps)(Header);
