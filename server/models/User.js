db59c66471eca9827bdc2cbb68ebcff5d5f8d5a3
drownedout <danielcdonnelly@gmail.com>
2018-08-29 12:57:41 -0700

    Added User and Article models

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+	name: String,
+	email: String,
+	provider: String,
+	provider_id: String,
+	token: String,
+	provider_pic: String,
+	followers: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		}
+	],
+	following: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		}
+	]
+});
+
+UserSchema.methods.follow = function(user_id){
+	if (this.following.indexOf(user_id) === -1){
+		this.following.push(user_id);
+	}
+	return this.save();
+}
+
+UserSchema.methods.addfollower = function(user){
+	this.followers.push(user)
+}
+
+module.exports = mongoose.model('User', UserSchema);
