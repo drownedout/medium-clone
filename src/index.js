eb55121becd11b09227aa056103278ca22289f53
drownedout <danielcdonnelly@gmail.com>
2018-08-29 14:12:39 -0700

    Created and wired up Redux store to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
+import { store, history } from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Provider store={store}>
+        <App />
+    </Provider>
+), document.getElementById('root'));
+
 registerServiceWorker();
