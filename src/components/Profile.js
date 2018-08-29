2efbc5a8ccda1eb4acb7179fe6f99976fde85e50
drownedout <danielcdonnelly@gmail.com>
2018-08-29 15:19:15 -0700

    Updated Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import FollowButton from './FollowButton';
+import { getUserProfile, follow } from './../redux/actions';
 
 class Profile extends Component {
+
+	componentDidMount(){
+		document.body.className = 'users show';
+	}
+
+	componentWillUnmount(){
+		document.body.className = '';
+	}
+
+	componentWillMount(){
+		this.props.getUserProfile(this.props.match.params.id)
+	}
+
 	render(){
 		return (
 			<div>
@@ -10,4 +26,16 @@ class Profile extends Component {
 	}
 }
 
-export default Profile;
+Profile.propTypes = {
+    params: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => {
+    return {
+        _article: state.articles.article,
+        user: state.authUser.user,
+        profile: state.authUser.profile
+    }
+}
+
+export default connect(mapStateToProps, { getUserProfile, follow})(Profile);
