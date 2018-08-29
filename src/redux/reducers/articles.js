da0dbbe3040ee0f7a5ab38f8f6f72a14c292b0f4
drownedout <danielcdonnelly@gmail.com>
2018-08-29 13:45:00 -0700

    Added articles reducer

---
+++ src/redux/reducers/articles.js
@@ -0,0 +1,28 @@
+const initialState = {
+	articles: [],
+	article: {}
+}
+
+export default (state=initialState, action){
+	switch(action.type){
+		case 'GET_ARTICLES':
+			return {
+				...state,
+				articles: action.articles
+			}
+		case 'GET_ARTICLE':
+			return {
+				...state,
+				article: action.article
+			}
+		case 'CLAP_ARTICLE':
+			let article = Object.assign({}, state.article);
+			article.claps++;
+			return {
+				..state,
+				article: article
+			}
+		default:
+			return state;
+	}
+}
